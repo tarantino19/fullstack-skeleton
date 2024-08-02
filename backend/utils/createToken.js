ea9f5caddfb9174c7938efa4eab32543223566da
tarantino19 <tolentinored19@gmail.com>
2024-08-02 19:56:35 +0800

    change password expiration timeframe

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (res, userId) => {
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: '30d',
+		expiresIn: '24h',
 	});
 
 	//Set JWT to http cookie only
